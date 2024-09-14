e8e56afc513c7e61afa0f437d629a627e658c1ea
Kevin_Merino <44932286+K3viin2@users.noreply.github.com>
2024-09-14 16:53:00 -0600

    Initial commit

---
+++ SQL SERVER/Stored Procedures.sql
@@ -0,0 +1,8 @@
+
+--Procedimiento Almacenado para Calcular la Correlación entre Rating y Age
+EXEC CalcularCorrelacionRatingEdad;
+
+
+--Procedimiento Almacenado para Calcular Estadísticas de Altura, Peso y Rating por Edad
+EXEC CalcularEstadisticasJugadores;
+
